764908b86867aa08b5b8b52e95566c170fae86a0
jimmy <3240429693@qq.com>
2022-10-05 17:00:24 +0800

    logistic init

---
+++ src/main/java/org/example/DataProcess.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.io.BufferedReader;
+
+public class DataProcess {
+
+    private String filePath="D:\\University\\junior\\学期实训\\课程相关材料\\Datasets\\AEEEM\\csv\\JDT.csv";
+    private BufferedReader br=null;
+    private String line="";
+    private String split=",";
+
+    
+
+}
